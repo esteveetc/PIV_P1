fe19cbd8a45c6278e366d93f5633a62e01726c9f
esteveetc <esteve.tc@gmail.com>
2022-10-11 12:25:11 +0200

    algo 1 acabat crec

--- algo1.m
+++ algo1.m
@@ -1,23 +1,80 @@
 %ALGO1 borrrrrrar
 
-im = imread('1_P_hgr1_id03_1.jpg');
+primeraFoto = 1;
 
-% passem de rgb a ycbcr
-YCBCRim = rgb2ycbcr(im);
-[Y Cb Cr] = imsplit(YCBCRim);
+% Especifiquem el directori de les imatges de training
+myFolder = 'DataBase Train-Valid Prog 1 i 2/DataBase Train-Valid Prog 1  i  2/Training-Dataset/Images/';
+
+% Fem una llista amb tots els fitxers .jpg
+filePattern = fullfile(myFolder, '*.jpg'); % Change to whatever pattern you need.
+
+theFiles = dir(filePattern);
+
+%recorrem els fitxers
+for k = 1 : length(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+    fprintf('Now processing %s\n', fullFileName);
+    im = imread(fullFileName);
+    
+    % passem de rgb a ycbcr
+    YCBCRim = rgb2ycbcr(im);
+    [Y Cb Cr] = imsplit(YCBCRim);
 
 
-CbArray = reshape(Cb,[],1);
-CrArray = reshape(Cr,[],1);
+    %Passem les matrius de crominancies a vectors
+    CbArray = double(reshape(Cb,[],1));
+    CrArray = double(reshape(Cr,[],1));
+    
+    %Fem que tots els vectors siguin de la mateixa mida per tal de poder
+    %operar amb ells
+    if primeraFoto == 1
+        CbTotal = CbArray;
+        CrTotal = CrArray;
+        primeraFoto = 0;
+    end
+    if length(CbTotal)>length(CbArray)
+       CbArray(end:numel(CbTotal))=0;
+    end
+    if length(CbArray)>length(CbTotal)
+       CbTotal(end:numel(CbArray))=0;
+    end
+    if length(CrTotal)>length(CrArray)
+       CrArray(end:numel(CrTotal))=0;
+    end
+    if length(CrArray)>length(CrTotal)
+       CrTotal(end:numel(CrArray))=0;
+    end
+    
+    
+    CbTotal = CbTotal+CbArray;
+    CrTotal = CrTotal+CrArray;
+end
 
-data = [CbArray,CrArray];
+%normalitzem els valors dels vectors de crominancia depenent de les fotos
+%analitzades
+CbTotal = CbTotal/length(theFiles);
+CrTotal = CrTotal/length(theFiles);
+CbRet = [];
+CrRet = [];
 
-[hcb] = histogram(CbArray);
-[hcr] = histogram(CrArray);
+%eliminem els valors que tenen crominancies molt diferents a la pell per
+%evitar tenir valors dels fondos de les imatges a l'histograma
+for i=1 : length(CbTotal)
+   if(CbTotal(i)>100 && CrTotal(i)>100)
+       CbRet(end + 1)=CbTotal(i);
+       CrRet(end + 1)=CrTotal(i);
+   end
+end
 
-X = [];
 
-%hist3(data);
+%Passem els valors dels vectors de crominancies a una matriu de parelles de valors
+data = [CbRet.',CrRet.'];
+
+%Creem l'histograma
+hist3(data,'Ctrs',{100:1:130 100:1:150},'CDataMode','auto','FaceColor','interp');
+
+
 
 
 
