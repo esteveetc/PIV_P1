a95cf9d5e8549b7ef12d116910271f1a47dd465f
Esteveetc <esteve.tc@gmail.com>
2022-12-19 15:55:05 +0100

    v3

--- algo5.m
+++ algo5.m
@@ -15,6 +15,11 @@ Dist1 = repmat(rescale(D), [1 1 3]); %creem una imatge amb 3dim
 %Guardem la mida de la imatge
 [rows, cols, ~]=size(D);
 
+Dcrop = imcrop(D,[cols/4,rows/4,cols*3/4,rows*3/4]);
+maxVal = max(Dcrop,[],'all');
+fprintf('v = %d\n',maxVal);
+
+
 umbralD = 10;
 
 %[R,G,B]=imsplit(Dist1);
@@ -32,16 +37,33 @@ for k=1 : rows
 end
 
 
-%SE = ones(16,16);
 
+
+%%%%% fer petit el petit se i fer un humbral mes, arreglar error fusio dits
+
+
+
+SE0 = ones(25,25);
+SE2 = ones(40,40);
+SE3 = ones(10,10);
+SE = SE0;
 SE1 = load('SE1.mat');
 
+if(maxVal<=50)
+    SE = SE1.SE;
+    fprintf('utilitzem el petit \n');
+    ImOp2 = imopen(D,SE);
+    
+else
+    SE = SE0;
+    fprintf('utilitzem el gran \n');
+    ImOp = imdilate(D,SE);
+    ImOp1 = imerode(ImOp,SE2);
+    ImOp2 = imopen(ImOp1,SE3);
+end
+    
 
-%ImDi = imdilate(D,SE1);
-ImOp = imopen(D,SE1.SE);
-%ImEr = imerode(ImOp,SE1);
-
-CC=bwconncomp(ImOp);
+CC=bwconncomp(ImOp2);
 n=CC.NumObjects;
 
 figure
@@ -51,7 +73,7 @@ subplot(2,2,2), imshow(Dist1), title('Euclidean')
 hold on,
 subplot(2,2,3), imshow(D), title('Umbral')
 hold on,
-subplot(2,2,4), imshow(ImOp), title('Opening')
+subplot(2,2,4), imshow(ImOp2), title('Opening')
 
 
 
